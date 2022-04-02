eb9f4580d6a258ba6ee454227d3d531d394c875f
sultonbayevogabek <sultonbayevogabek@gmail.com>
2022-04-02 22:48:55 +0500

    nimadir

--- script.js
+++ script.js
@@ -1,82 +1,82 @@
-const listGroup = document.querySelector(".list-group");
-const createUserFormI = document.querySelector("#create-form");
-const firstNameInputI = createUserForm.querySelector("#first_name");
-const lastNameInputI = createUserForm.querySelector("#last_name");
-const emailInputI = createUserForm.querySelector("#email");
-const birthdateInputI = createUserForm.querySelector("#birthdate");
-const usernameInputI = createUserForm.querySelector("#username");
-const phoneInputI = createUserForm.querySelector("#phone");
-const messageI = createUserForm.querySelector(".message");
+'use strict'
 
-const editUserForm = document.querySelector("#edit-form");
-const idInputE = editUserForm.querySelector("#id");
-const firstNameInputE = editUserForm.querySelector("#first_name_e");
-const lastNameInputE = editUserForm.querySelector("#last_name_e");
-const emailInputE = editUserForm.querySelector("#email_e");
-const birthdateInputE = editUserForm.querySelector("#birthdate_e");
-const usernameInputE = editUserForm.querySelector("#username_e");
-const phoneInputE = editUserForm.querySelector("#phone_e");
+const listGroup = document.querySelector('.list-group')
+const createUserForm = document.querySelector('#create-form')
+const firstNameInput = createUserForm.querySelector('#first_name')
+const lastNameInput = createUserForm.querySelector('#last_name')
+const emailInput = createUserForm.querySelector('#email')
+const birthdateInput = createUserForm.querySelector('#birthdate')
+const usernameInput = createUserForm.querySelector('#username')
+const phoneInput = createUserForm.querySelector('#phone')
+const message = createUserForm.querySelector('.message')
 
-createUserForm.addEventListener("submit", async (e) => {
-  e.preventDefault();
+const editUserForm = document.querySelector('#edit-form')
+const idInputE = editUserForm.querySelector('#id')
+const firstNameInputE = editUserForm.querySelector('#first_name_e')
+const lastNameInputE = editUserForm.querySelector('#last_name_e')
+const emailInputE = editUserForm.querySelector('#email_e')
+const birthdateInputE = editUserForm.querySelector('#birthdate_e')
+const usernameInputE = editUserForm.querySelector('#username_e')
+const phoneInputE = editUserForm.querySelector('#phone_e')
 
-  let newUser = {
-    first_name: firstNameInput.value,
-    last_name: lastNameInput.value,
-    email: emailInput.value,
-    birthdate: birthdateInput.value,
-    username: usernameInput.value,
-    phone_number: phoneInput.value,
-  };
+createUserForm.addEventListener('submit', async e => {
+    e.preventDefault()
 
-  let response = await fetch("http://localhost:3000/create", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(newUser),
-  });
-  response = await response.json();
-  await getUserList();
-  message.innerHTML = `<div class="alert text-center ${
-    response.message === "New user created" ? "alert-success" : "alert-warning"
-  }">${response.message}</div>`;
-});
+    let newUser = {
+        first_name: firstNameInput.value,
+        last_name: lastNameInput.value,
+        email: emailInput.value,
+        birthdate: birthdateInput.value,
+        username: usernameInput.value,
+        phone_number: phoneInput.value
+    }
 
-editUserForm.addEventListener("submit", async (e) => {
-  e.preventDefault();
+    let response = await fetch('http://localhost:3000/create', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newUser)
+    })
+    response = await response.json()
+    await getUserList()
+    message.innerHTML = `<div class="alert text-center ${response.message === 'New user created' ? 'alert-success' : 'alert-warning'}">${ response.message }</div>`
+})
 
-  const editedData = {
-    id: idInputE.value,
-    first_name: firstNameInputE.value,
-    last_name: lastNameInputE.value,
-    email: emailInputE.value,
-    birthdate: birthdateInputE.value,
-    username: usernameInputE.value,
-    phone_number: phoneInputE.value,
-  };
+editUserForm.addEventListener('submit', async e => {
+    e.preventDefault()
 
-  let response = await fetch("http://localhost:3000/edit", {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    method: "POST",
-    body: JSON.stringify(editedData),
-  });
-  response = await response.json();
-  if (response.message === "User edited") {
-    window.location.reload();
-  }
-});
+    const editedData = {
+        id: idInputE.value,
+        first_name: firstNameInputE.value,
+        last_name: lastNameInputE.value,
+        email: emailInputE.value,
+        birthdate: birthdateInputE.value,
+        username: usernameInputE.value,
+        phone_number: phoneInputE.value
+    }
+
+    let response = await fetch('http://localhost:3000/edit', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(editedData)
+    })
+    response = await response.json()
+    if (response.message === 'User edited') {
+        window.location.reload()
+    }
+})
 
 async function getUserList() {
-  let response = await fetch("http://localhost:3000/users", {
-    method: "GET",
-  });
-  response = await response.json();
-  listGroup.innerHTML = "";
-  response.users.forEach((item) => {
-    listGroup.innerHTML += `
+    let response = await fetch('http://localhost:3000/users', {
+        method: 'GET'
+    })
+    response = await response.json()
+    listGroup.innerHTML = ''
+    response.users.forEach(item => {
+        listGroup.innerHTML += `
             <li class="list-group-item d-flex justify-content-between">
                 <div>
                     <p class="mb-1"><strong>Firstname: </strong><span>${item.first_name}</span></p>
@@ -90,48 +90,48 @@ async function getUserList() {
                     <button class="btn btn-danger" data-delete="${item.id}">Delete</button>
                 </div>
             </li>
-        `;
-  });
-  deleteUser();
-  editUser();
+        `
+    })
+    deleteUser()
+    editUser()
 }
 
 function deleteUser() {
-  const deleteButtons = document.querySelectorAll("[data-delete]");
-  deleteButtons.forEach((btn) => {
-    btn.addEventListener("click", async (e) => {
-      const id = e.target.getAttribute("data-delete");
-      let response = await fetch("http://localhost:3000/delete/" + id, {
-        method: "DELETE",
-      });
-      response = await response.json();
+    const deleteButtons = document.querySelectorAll('[data-delete]')
+    deleteButtons.forEach(btn => {
+        btn.addEventListener('click', async e => {
+            const id = e.target.getAttribute('data-delete')
+            let response = await fetch('http://localhost:3000/delete/' + id, {
+                method: 'DELETE'
+            })
+            response = await response.json()
 
-      if (response.message === "User deleted") {
-        window.location.reload();
-      }
-    });
-  });
+            if (response.message === 'User deleted') {
+                window.location.reload()
+            }
+        })
+    })
 }
 
 function editUser() {
-  const editButtons = document.querySelectorAll("[data-edit]");
-  editButtons.forEach((btn) => {
-    btn.addEventListener("click", async (e) => {
-      const id = e.target.getAttribute("data-edit");
-      let response = await fetch("http://localhost:3000/users/" + id);
-      response = await response.json();
+    const editButtons = document.querySelectorAll('[data-edit]')
+    editButtons.forEach(btn => {
+        btn.addEventListener('click', async e => {
+            const id = e.target.getAttribute('data-edit')
+            let response = await fetch('http://localhost:3000/users/' + id)
+            response = await response.json()
 
-      idInputE.value = response.user.id;
-      firstNameInputE.value = response.user.first_name;
-      lastNameInputE.value = response.user.last_name;
-      emailInputE.value = response.user.email;
-      phoneInputE.value = response.user.phone_number;
-      birthdateInputE.value = response.user.birthdate;
-      usernameInputE.value = response.user.username;
-    });
-  });
+            idInputE.value = response.user.id
+            firstNameInputE.value = response.user.first_name
+            lastNameInputE.value = response.user.last_name
+            emailInputE.value = response.user.email
+            phoneInputE.value = response.user.phone_number
+            birthdateInputE.value = response.user.birthdate
+            usernameInputE.value = response.user.username
+        })
+    })
 }
 
-(async (_) => {
-  await getUserList();
-})();
+;(async _ => {
+    await getUserList()
+})()
